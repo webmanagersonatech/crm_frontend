960f7f018685069a04815fe491a2dfcb270c0bdf
vinoth <webdeveloper2@sonatech.ac.in>
2026-09-22 15:54:48 +0530

    pushh

--- app/(protected)/fees/page.tsx
+++ app/(protected)/fees/page.tsx
@@ -1587,7 +1587,7 @@ export default function FeeStructurePage() {
                       onClick={handleAddReferral}
                       className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
                     >
-                      + Add Referral
+                      + Add Concession
                     </button>
                   )}
                 </div>
